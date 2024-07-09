11345e1295149f8ab5c79faabbde774db041e27a
valeriaAnabeldePrado <anabeltayu@gmail.com>
2024-07-08 21:22:02 -0300

    arreglando el hero para el seo

--- src/app/componentes/cuerpo/hero.jsx
+++ src/app/componentes/cuerpo/hero.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import ResizeW from "ana/app/redux/useResize";
 import ScDesktop from "./scDesktop";
 import ScTelephone from "./scTelephone";
+import Image from "next/image";
 
 const Hero = () => {
   const heroRef = useRef(null);
@@ -21,12 +22,24 @@ const Hero = () => {
     <>
       <ResizeW>
         <div className="hero-cuerpo" ref={heroRef}>
-          <img src="./esferaOsc.webp" alt="esfera" className="esfera esfera1" />
-          <img
-            src="./blopesfera.webp"
-            alt="esfera"
-            className="esfera esfera2"
-          />
+          <div className="contenedor_img_esferas_uno">
+            <Image
+              src="/esferaOsc.webp"
+              alt="esfera"
+              className="esfera esfera1"
+              width={100}
+              height={110}
+            />
+          </div>
+          <div className="contenedor_img_esferas_dos">
+            <Image
+              src="/blopesfera.webp"
+              alt="esfera"
+              className="esfera esfera2"
+              width={200}
+              height={220}
+            />
+          </div>
           <h4 className="textoHero">
             La innovación, el diseño y la tecnología se entrelazan para crear
             experiencias digitales que desafían lo convencional. Somos más que
