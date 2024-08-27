97ec0c70f99b4975d5826a8a48762c67abaefd03
valeriaAnabeldePrado <anabeltayu@gmail.com>
2024-08-27 11:29:16 -0300

    finalizando web

--- src/app/componentes/cuerpo/presentacion.jsx
+++ src/app/componentes/cuerpo/presentacion.jsx
@@ -9,25 +9,18 @@ const Presentacion = () => {
           <h2>hacemos?</h2>
         </div>
         <p className="presentacion-texto">
-          En SmartCloud, nos sumergimos en la intersección del diseño y la
-          tecnología. Nuestro enfoque multidisciplinario abarca diseño UX/UI,
-          desarrollo de Software a medida, Branding impactante, producciones
-          audiovisuales de alta calidad, renderizado 3D fotorrealista, entre
-          otros servicios. Nuestra pasión por la calidad y la innovación nos
-          distingue.
-          <strong style={{ backgroundColor: "transparent" }}>
-            Somos la opción para aquellos que buscan sobresalir en un mundo
-            digital saturado, porque lo estándar no es suficiente.
+          En SmartCloud, combinamos diseño y tecnología, ofreciendo servicios
+          como diseño UX/UI, desarrollo de software a medida, branding,
+          producciones audiovisuales de alta calidad y renderizado 3D. Nos
+          destacamos por nuestra pasión por la calidad y la innovación
+          <strong className="m-2" style={{ backgroundColor: "transparent" }}>
+            Somos la opción para quienes quieren destacar en un mundo digital
+            saturado; lo estándar no basta.
           </strong>
-          Creemos en el poder de la tecnología para transformar negocios y
-          conectarse con las audiencias de manera significativa. Nuestro equipo
-          proviene de Córdoba y Neuquén, reuniendo una diversa gama de
-          perspectivas y experiencias que enriquecen nuestro proceso creativo.
-          Somos disruptores en la industria, brindamos soluciones creativas y
-          técnicamente superiores que desafían las normas convencionales.
-          Nuestro enfoque centrado en el cliente y nuestra mentalidad visionaria
-          nos permiten forjar interacciones digitales excepcionales que marcan
-          la diferencia.
+          Transformamos negocios con tecnología que conecta profundamente con
+          las audiencias. Nuestro equipo ofrece soluciones innovadoras y
+          técnicamente avanzadas. Desafiamos lo convencional para crear
+          experiencias digitales excepcionales.
         </p>
       </section>
     </>
